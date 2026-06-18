9a97cc00cc959d74206f8b4bf9b7c1a037854070
Mateo Grueso <mateogrueso10@gmail.com>
2026-06-17 21:24:29 -0400

    added button listener events

--- Assignment-1/js/story.js
+++ Assignment-1/js/story.js
@@ -20,3 +20,43 @@ const btn2 = document.getElementById("btn2");
 const btn3 = document.getElementById("btn3");
 const btn4 = document.getElementById("btn4");
 const btn5 = document.getElementById("btn5");
+
+// Get story display
+const storyBtn = document.getElementById("storyBtn");
+const storyOutput = document.getElementById("story");
+
+// button 1
+btn1.addEventListener("click", () => {
+    animalIndex = (animalIndex + 1) % animals.length;
+    btn1.textContent = animals[animalIndex];
+});
+
+// button 2
+btn2.addEventListener("click", () => {
+    actionIndex = (actionIndex + 1) % actions.length;
+    btn2.textContent = actions[actionIndex];
+});
+
+// button 3
+btn3.addEventListener("click", () => {
+    placeIndex = (placeIndex + 1) % places.length;
+    btn3.textContent = places[placeIndex];
+});
+
+// button 4
+btn4.addEventListener("click", () => {
+    objectIndex = (objectIndex + 1) % objects.length;
+    btn4.textContent = objects[objectIndex];
+});
+
+// button 5
+btn5.addEventListener("click", () => {
+    endingIndex = (endingIndex + 1) % endings.length;
+    btn5.textContent = endings[endingIndex];
+});
+
+// story button
+storyBtn.addEventListener("click", () => {
+    const story = `The ${animals[animalIndex]} ${actions[actionIndex]} in the ${places[placeIndex]} with a ${objects[objectIndex]} ${endings[endingIndex]}.`;
+    storyOutput.textContent = story;
+});
