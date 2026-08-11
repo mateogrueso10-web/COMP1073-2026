d0ba92b1bc59bc2bf5946728dc4a6b063287ec92
Mateo Grueso <mateogrueso10@gmail.com>
2026-08-11 16:11:41 -0400

    added student info and html elements

--- Assignment-3/js/script.js
+++ Assignment-3/js/script.js
@@ -11,3 +11,35 @@ const API_KEY = "MbgEtRt2bLJvcMWln7WiJbjl1EzI1HHkV1qMKjax";
 
 // NASA APOD API URL.
 const API_URL = "https://api.nasa.gov/planetary/apod";
+
+/*  Student Information  */
+
+// Find the paragraph from the HTML.
+const studentInfo = document.getElementById("studentInfo");
+
+// Add student information dynamically using JavaScript.
+studentInfo.textContent =
+    "Student: Mateo Grueso | Student ID: 200655020";
+
+/*  Get HTML Elements  */
+
+const datePicker =
+    document.getElementById("datePicker");
+
+const searchButton =
+    document.getElementById("searchButton");
+
+const randomButton =
+    document.getElementById("randomButton");
+
+const loading =
+    document.getElementById("loading");
+
+const errorMessage =
+    document.getElementById("errorMessage");
+
+const featuredContainer =
+    document.getElementById("featuredContainer");
+
+const gallery =
+    document.getElementById("gallery");
