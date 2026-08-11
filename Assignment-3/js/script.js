0f1a436ca8c2fd52ca1929a02fea0ddeec451e86
Mateo Grueso <mateogrueso10@gmail.com>
2026-08-11 16:09:21 -0400

    added the API KEY

--- Assignment-3/js/script.js
+++ Assignment-3/js/script.js
@@ -4,4 +4,10 @@
    NASA Astronomy Picture of the Day API
  */
 
-   
+/*  API Settings  */
+
+// Replace this with your NASA API key.
+const API_KEY = "MbgEtRt2bLJvcMWln7WiJbjl1EzI1HHkV1qMKjax";
+
+// NASA APOD API URL.
+const API_URL = "https://api.nasa.gov/planetary/apod";
